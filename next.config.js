f07181f54d8600ef4e3c46e4baa2f790a731006a
trungbach <66735837+trungbach@users.noreply.github.com>
2021-07-28 15:43:11 +0700

    nextroung

--- next.config.js
+++ next.config.js
@@ -8,17 +8,17 @@ module.exports = {
   images: {
     domains: ['opensea.io', "storage.googleapis.com", "lh3.googleusercontent.com"],
   },
-  exportTrailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
-    }
-  },
+  trailingSlash: true,
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //     '/about': { page: '/about' },
+  //     '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+  //     '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+  //     '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+  //   }
+  // },
 }
